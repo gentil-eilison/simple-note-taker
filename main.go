cb4390eea5106510c5bc028a67707d66488c11a3
Gentil Ályson <gentil.alyson@nees.ufal.br>
2024-05-17 14:39:58 -0300

    feature: finishes note taking app

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
 	"example.com/note/note"
 )
@@ -16,18 +19,34 @@ func main() {
 	}
 
 	note.Display()
+	err = note.Save()
 
+	if err != nil {
+		fmt.Println("Saving the note failed")
+		return
+	}
+
+	fmt.Println("Saving the note succeeded")
 }
 
 func getNoteData() (string, string) {
-	title := getUserInput("Note title: ")
-	content := getUserInput("Note content: ")
+	title := getUserInput("Note title:")
+	content := getUserInput("Note content:")
 	return title, content
 }
 
 func getUserInput(prompt string) string {
-	fmt.Print(prompt)
-	var value string
-	fmt.Scanln(&value)
-	return value
+	fmt.Printf("%v ", prompt)
+
+	reader := bufio.NewReader(os.Stdin) // new reader from standard input (the cli)
+	text, err := reader.ReadString('\n')
+
+	if err != nil {
+		return ""
+	}
+
+	text = strings.TrimSuffix(text, "\n")
+	text = strings.TrimSuffix(text, "\r") // needed for Windows-based input
+
+	return text
 }
--- note/note.go
+++ note/note.go
@@ -1,19 +1,35 @@
 package note
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"os"
+	"strings"
 	"time"
 )
 
 type Note struct {
-	title     string
-	content   string
-	createdAt time.Time
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 func (note Note) Display() {
-	fmt.Printf("Note title: %v\n\nNote description: %v\n", note.title, note.content)
+	fmt.Printf("Your note titled %v has the following content:\n\n%v\n", note.Title, note.Content)
+}
+
+func (note Note) Save() error {
+	fileName := strings.ReplaceAll(note.Title, " ", "_")
+	fileName = strings.ToLower(fileName) + ".json"
+
+	json, err := json.Marshal(note)
+
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(fileName, json, 0644)
 }
 
 func New(title, content string) (Note, error) {
@@ -22,8 +38,8 @@ func New(title, content string) (Note, error) {
 	}
 
 	return Note{
-		title:     title,
-		content:   content,
-		createdAt: time.Now(),
+		Title:     title,
+		Content:   content,
+		CreatedAt: time.Now(),
 	}, nil
 }
